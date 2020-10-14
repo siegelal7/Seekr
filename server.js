52c5568206c7b4695cb008b8682f4e41e077dacc
Andrew Siegel <siegelal7@gmail.com>
2020-10-14 15:17:47 -0400

    trying to get edit form to prepopulate

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.static(path.join(__dirname, "/public")));
 // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
 // We need to use sessions to keep track of our user's login status TODO: Figure out wtf keyboard cat is
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -47,9 +49,8 @@ app.use(jobsRoutes);
 const usersRoutes = require("./controllers/usersController");
 app.use(usersRoutes);
 
-
-db.sequelize.sync({ force: true }).then(function () {
-// db.sequelize.sync().then(function () {
+// db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, () => {
     console.log("Server listening on http://localhost:" + PORT);
   });
