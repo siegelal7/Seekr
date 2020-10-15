1fc19424956a607d60802fea48aed583f4130fb2
Andrew Siegel <siegelal7@gmail.com>
2020-10-14 20:00:14 -0400

    removed some of the javascript from job-board.handlebars

--- public/assets/drag-n-drop.js
+++ public/assets/drag-n-drop.js
@@ -1,58 +1,127 @@
 $(document).ready(function () {
-  //   console.log("test");
-  // const newJobModal = $("#newJobModal");
-  // const addJob = $("#submit-new-job-form");
-  // addJob.on("submit", function (event) {
-  //   event.preventDefault();
-  //   console.log("test");
-  //   const obj = {
-  //     jobName: $("#job-title").val().trim(),
-  //     company: $("#company-name").val().trim(),
-  //     position: $("#position").val().trim(),
-  //     stage: $("#stage").val().trim(),
-  //   };
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "api/job",
-  //     data: obj,
-  //   }).then(function (data) {
-  //     console.log(data);
-  //     location.reload();
-  //   });
-  //not sure this is the best solution to hide it
-  // newJobModal.attr("style", "display:none");
-  // });
+  const addNewProspect = $(".addNewProspect");
+  let currentStage;
+  addNewProspect.on("click", function (event) {
+    currentStage = $(this).val();
+  });
+
+  let currentUserId;
+  $.get("/api/user_data").then((dbUser) => {
+    currentUserId = dbUser.id;
+  });
+  //event listener for add new job btn
+  // const newJobModal = $("#exampleModal1");
+  const addJob = $("#submit-new-job-form");
+
+  addJob.on("submit", function (event) {
+    event.preventDefault();
+    const obj = {
+      jobName: $("#job-title").val().trim(),
+      company: $("#company-name").val().trim(),
+      position: $("#position").val().trim(),
+      stage: currentStage,
+      UserId: currentUserId,
+    };
+    $.ajax({
+      type: "POST",
+      url: "api/job",
+      data: obj,
+    }).then(function (data) {
+      console.log(data);
+      location.reload();
+    });
+    //not sure this is the best solution to hide it
+    // newJobModal.attr("style", "display:none");
+  });
+  let targetTitle;
+  let targetId;
+  const editBtn = $(".updateBtn");
+  editBtn.on("click", function (event) {
+    console.log(event.target.parentNode.parentNode.childNodes);
+    //console.log(event.target.parentNode.getAttribute("data-id"));
+    targetId = event.target.parentNode.getAttribute("data-id");
+    //console.log(targetId);
+  });
+
+  //update form submit event listener
+  const editForm = $("#editForm");
+  editForm.on("submit", (event) => {
+    event.preventDefault();
+    // console.log(targetId);
+    // console.log("clicked");
+    // const currentId = event.currentTarget.parentNode.getAttribute("data-id");
+    // console.log(currentId);
+    const obj = {
+      jobName: $("#job-title-edit").val().trim(),
+      company: $("#company-name-edit").val().trim(),
+      position: $("#position-edit").val().trim(),
+      stage: $("#stage-edit").val().trim(),
+      id: targetId,
+    };
+    $.ajax({
+      url: "api/job",
+      method: "PUT",
+      data: obj,
+    }).then(function (response) {
+      console.log("successfully updated entry");
+      location.reload();
+    });
+  });
+
+  //delete btn event listener
+  const deleteBtn = $(".deleteBtn");
+  deleteBtn.on("click", function (event) {
+    targetId = event.currentTarget.parentNode.getAttribute("data-id");
+    $.ajax({
+      url: `api/job/${targetId}`,
+      type: "DELETE",
+      data: { id: targetId },
+    }).then(function (response) {
+      console.log("successfully deleted " + targetId + " job");
+      location.reload();
+    });
+  });
+
   // const dragStart = (event) => {
   //   event.currentTarget.classList.add("dragging");
   // };
+
   // const dragEnd = (event) => {
   //   event.currentTarget.classList.remove("dragging");
   // };
+
   // Array.from(document.querySelectorAll(".card")).forEach((card) => {
   //   card.addEventListener("dragstart", dragStart);
   //   card.addEventListener("dragend", dragEnd);
   // });
+
   // const drag = (event) => {
   //   event.dataTransfer.setData("text/html", event.currentTarget.outerHTML);
   //   event.dataTransfer.setData("text/plain", event.currentTarget.dataset.id);
   // };
+
   // const dragEnter = (event) => {
   //   event.currentTarget.classList.add("drop");
   // };
+
   // const dragLeave = (event) => {
   //   event.currentTarget.classList.remove("drop");
   // };
+
   // Array.from(document.querySelectorAll(".column")).forEach((column) => {
   //   column.addEventListener("dragenter", dragEnter);
   //   column.addEventListener("dragleave", dragLeave);
   // });
+
   // const drop = (event) => {
   //   Array.from(document.querySelectorAll(".column")).forEach((column) =>
   //     column.classList.remove("drop")
   //   );
+
   //   document
   //     .querySelector(`[data-id="${event.dataTransfer.getData("text/plain")}"]`)
   //     .remove();
+
   //   event.currentTarget.innerHTML =
   //     event.currentTarget.innerHTML + event.dataTransfer.getData("text/html");
   //   //console.log(event.currentTarget.lastElementChild.getAttribute("data-id"))
@@ -74,12 +143,12 @@ $(document).ready(function () {
   //   } else if (stage == "rejected") {
   //     newStage = "Rejected";
   //   }
-  //   // console.log("NEWSTAGE: " + newStage);
+  //   console.log("NEWSTAGE: " + newStage);
   //   const data = {
   //     stage: newStage,
   //     id: id,
   //   };
-  //   // console.log("ID: " + id);
+  //   console.log("ID: " + id);
   //   $.ajax({
   //     url: "api/job",
   //     type: "PUT",
@@ -89,8 +158,13 @@ $(document).ready(function () {
   //     location.reload();
   //   });
   // };
+
   // const allowDrop = (event) => {
   //   event.preventDefault();
   //   // console.log(event.currentTarget.lastElementChild.getAttribute("data-id"));
   // };
+
+  $.get("/api/user_data").then((dbUser) => {
+    $(".user-name").text(dbUser.firstName);
+  });
 });
