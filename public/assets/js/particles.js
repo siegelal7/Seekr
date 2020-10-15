b802ddfbe924d71930f0dfe3ac17a5cb3a6c17f9
David Pan <dpan5425@gmail.com>
2020-10-15 01:26:44 -0400

    login

--- public/assets/js/particles.js
+++ public/assets/js/particles.js
@@ -4,7 +4,7 @@ particlesJS("particles-js", {
         "value": 45,
         "density": {
           "enable": true,
-          "value_area": 800
+          "value_area": 800 
         }
       },
       "color": {
